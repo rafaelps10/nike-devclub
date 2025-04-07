bb744ba2a5d7e49c3fa1041ceb3a3f7e2bd3de21
Bowie <rafael.ps.skt@gmail.com>
2025-04-07 13:25:38 -0300

    retirado comentários

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
-// document = html
-// querySelector = pegador / selecionador
 
 function mudarVisual(cor, imagem){
     
